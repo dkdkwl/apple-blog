23f4b463a31cff2b23a8590205fbd0818357b7d1
dkdkwl <dkdkwl@naver.com>
2022-11-10 14:16:31 +0900

    map함수 돌려서 list만들기

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
 
   let [글제목,글제목변경] = useState(['남자 코트 추천','강남 우동 맛집','파이썬독학']);
-  let [따봉,따봉변경] = useState(0);
+  let [따봉,따봉변경] = useState([0,1,2]);
   let [modal,setModal] = useState(false);
 
   return (
@@ -24,15 +24,11 @@ function App() {
         copy[0] = '여자코트 추천'
           글제목변경(copy);
         }}>글제목변경</button>
-
-      <div className='list'>
+      {/* <div className='list'>
         <h4>{글제목[0]}<span onClick={()=>{ 따봉변경(따봉+1);}}>🤣</span>{따봉}</h4>
         <p>2월 17일 발행</p>
       </div>
-      <div className='list'>
-        <h4>{글제목[1]}</h4>
-        <p>2월 17일 발행</p>
-      </div>
+
       <div className='list'>
         <h4 onClick={
           ()=>{setModal(!modal);
@@ -41,7 +37,24 @@ function App() {
           modal == true ? <Modal></Modal> : null
         }
         <p>2월 17일 발행</p>
-      </div>
+      </div> */}
+
+      {
+        글제목.map(function(a,i){
+          console.log(따봉[i]);
+          return (
+            <div className='list' key={i}>
+            <h4>{글제목[i]}<span onClick={
+              ()=>{
+                let copy = [...따봉];
+                copy[i] = copy[i]+1;
+                따봉변경(copy);
+              }}>🤣</span>{따봉[i]}</h4>
+            <p>2월 17일 발행</p>
+          </div>
+          )
+        })
+      }
     </div>
   );
 }
